a25a19793638ef2659adf6c6a44b12cc4685d1c7
Dhatripatel24 <dhatriptl@gmail.com>
2024-08-20 16:29:14 +0530

    update

---
+++ Navigator/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Navabar from './components/Navabar'
+import Privatepage from './components/Privatepage'
+import './App.css'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Navabar/>
+       
+    </>
+  )
+}
+
+export default App
