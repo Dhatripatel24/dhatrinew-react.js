dd7815917d29ebe09860e5eb39e5b49191d11c92
Dhatripatel24 <dhatriptl@gmail.com>
2024-09-18 16:33:45 +0530

    update

--- Redux Builder/counter/src/Components/Counter.jsx
+++ Redux Builder/counter/src/Components/Counter.jsx
@@ -1,23 +1,6 @@
 
 
-// // export default Counter;
-// // Counter.jsx
-// import React from 'react';
-// import CounterValue from './CounterValue';
-// import CounterButtons from './CounterButtons';
 
-
-// const Counter = () => {
-//   return (
-//     <div>
-//       <CounterValue />
-//       <CounterButtons/>
-//     </div>
-//   );
-// };
-
-// export default Counter;
-// Counter.jsx
 import React from 'react';
 import CounterValue from './CounterValue';
 import CounterButtons from './CounterButtons';
